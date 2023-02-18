9c71672579f89ced16a00e23f8b96f4af28ccc01
Hieu194281 <trunghieu220801@gmail.com>
2023-02-18 15:21:34 +0700

    HideShow

--- src/component/DisplayInfo.js
+++ src/component/DisplayInfo.js
@@ -1,17 +1,35 @@
 import React from "react";
 
 class DisplayInfo extends React.Component {
+
+    state = {
+        isShowList: true
+    }
+    handleShowHide() {
+        this.setState({
+            isShowList: !this.state.isShowList
+        })
+    }
     render() {
         // desctructuring array
         const { listUsers } = this.props
+        console.log(listUsers)
         return (
             <div>
+                <div>
+                    <span onClick={() => { this.handleShowHide() }}>
+                        {this.state.isShowList === true ? "Hide List User" : "Show List User"}
+                    </span>
+                </div>
                 {listUsers.map((user) => {
                     return (
                         <div key={user.id}>
-                            <div>My name is {user.name}</div>
-                            <div>My age is {user.age}</div>
-                            <hr></hr>
+                            {this.state.isShowList &&
+                                <div key={user.id} className={user.age > 18 ? 'red' : 'green'}>
+                                    <div>My name is {user.name}</div>
+                                    <div>My age is {user.age}</div>
+                                    <hr></hr>
+                                </div>}
                         </div>
                     )
                 })}
--- src/component/MyComponent.js
+++ src/component/MyComponent.js
@@ -11,7 +11,7 @@ class MyComponent extends React.Component {
         listUsers: [
             { id: 1, name: 'Hoi dan IT', age: 30 },
             { id: 2, name: 'Hoi Hieu IT', age: 10 },
-            { id: 3, name: 'Hieu IT', age: 20 }
+            { id: 3, name: 'Hieu IT', age: 24 }
         ]
     }
 
