ca721274c4a6f056118e67a8712b9bb8c50bb303
Hieu194281 <trunghieu220801@gmail.com>
2023-02-18 14:31:15 +0700

    listUser

--- src/component/MyComponent.js
+++ src/component/MyComponent.js
@@ -7,19 +7,22 @@ import UserInfo from "./UserInfo";
 
 class MyComponent extends React.Component {
 
+    state = {
+        listUsers: [
+            { id: 1, name: 'Hoi dan IT', age: 30 },
+            { id: 2, name: 'Hoi Hieu IT', age: 10 },
+            { id: 3, name: 'Hieu IT', age: 20 }
+        ]
+    }
 
     //JSX
     render() {
-        const myInfo = ['abc', 'adsaca']
         return (
             <div>
                 <UserInfo></UserInfo>
                 <br></br>
                 <br></br>
-                <DisplayInfo name='Hoi dan IT' age='30'></DisplayInfo>
-                <hr></hr>
-                <DisplayInfo name='Hoi Hieu IT' age={6} myInfo={myInfo}></DisplayInfo>
-
+                <DisplayInfo listUsers={this.state.listUsers} ></DisplayInfo>
             </div>
         )
     }
